4796264f55b1099494df06130cc8d881db7195f2
Guntbert Reiter <guntbert@gmx.at>
2022-10-29 22:14:54 +0200

    Task 4 (City) WIP

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -510,6 +510,30 @@ After defining the city, implement the following functions:
    and at least 10 living __people__ inside in all houses of the city in total.
 -}
 
+data House = House
+  { housePeopleCount :: Int} deriving (Show)
+
+data Castle = Castle
+  { castleName :: String}
+  deriving (Show)
+
+data Wall = Wall deriving Show
+
+data CultureInstitution
+  = Church
+  | Library
+  deriving Show
+
+data MagicalCity = MagicalCity
+  { cityName :: String
+  , cityCastle :: Castle
+  , cityWall :: Wall
+  , cityCI :: CultureInstitution
+  , houses :: [House]} deriving (Show)
+
+{- buildCastle :: MagicalCity -> String -> MagicalCity
+buildCastle city cn = city { cityCastle {castleName = cn}} -}
+
 {-
 =🛡= Newtypes
 
